c1633b3d0023a82e81be145497a2368a82415dd1
GH-TimG <tim.grenzhaeuser@gmail.com>
2023-02-19 05:25:05 +0100

    Create React Template

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome!</h1>
+      <p><b>-Under Construction-</b><br />Please be patient :)</p>
+    </div>
+  );
+}
+
+export default App;
